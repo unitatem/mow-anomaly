4bbefe94446d315113c06d761c531288b8156428
Mariusz Wachowicz <wachowiczmariusz@gmail.com>
2018-04-30 01:22:19 +0200

    Read creadit card data file

---
+++ src/main.R
@@ -0,0 +1,13 @@
+print("START")
+
+resources_path = "../resources/"
+credit_card_file = "creditcard.csv"
+
+creadit_card_path = paste(resources_path, credit_card_file, sep = "")
+data = read.csv(file = creadit_card_path,
+                header = TRUE,
+                sep = ",")
+
+print(head(data, 3))
+
+print("END")
