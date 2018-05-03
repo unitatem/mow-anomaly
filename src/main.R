b0f097afb4abdc38cdd7ca28a235667ac0130c2f
Mariusz Wachowicz <wachowiczmariusz@gmail.com>
2018-05-03 20:29:16 +0200

    Encapsulte script in functions

--- src/main.R
+++ src/main.R
@@ -1,5 +1,3 @@
-source("src/config.R")
-
 read_csv_file = function(path) {
     data = read.csv(file = path,
                     header = TRUE,
@@ -7,46 +5,43 @@ read_csv_file = function(path) {
     return(data)
 }
 
-# closure, awesome
-wrap_function_over_columns = function(lambda) {
-    embeded_function = function(data) {
-        cols_cnt = ncol(data)
-        results = 1:cols_cnt
-        for (c in 1:cols_cnt) {
-            results[c] = lambda(data_safe[,c])
-        }
-        return(results)
-    }
-    return(embeded_function)
+extract_noraml_anomaly = function(mixed_data) {
+    # first column is ID, second column is transaction time, last column is class
+    idx_col = 3:(ncol(mixed_data) - 1)
+
+    data = list(normal = 0,
+                anomaly = 0,
+                normal_nrow = 0)
+
+    data$normal = mixed_data[mixed_data$Class == 0, idx_col]
+    data$anomaly = mixed_data[mixed_data$Class == 1, idx_col]
+
+    data$normal_nrow = nrow(data$normal)
+
+    return(data)
 }
 
-independent_probability = function(data, means, std) {
-    rows_cnt = nrow(data)
-    cols_cnt = ncol(data)
+extract_training_test = function(normal_anomaly) {
+    perm_vect = sample(1:normal_anomaly$normal_nrow)
+    divide_idx = nrow(normal_anomaly$anomaly)
 
-    result = 1:rows_cnt
-    for (r in 1:rows_cnt) {
-        result[r] = 1
-        for (c in 1:cols_cnt)
-            result[r] = result[r] * dnorm(data[r,c], mean = means[c], sd = deviations[c])
-    }
+    idx_normal_2_test = perm_vect[1:divide_idx]
+    idx_normal_2_training = perm_vect[(divide_idx + 1):normal_anomaly$normal_nrow]
 
+    data_training = list(normal = normal_anomaly$normal[idx_normal_2_training, ])
+
+    data_test = list(normal = normal_anomaly$normal[idx_normal_2_test,],
+                     anoamaly = normal_anomaly$anomaly)
+
+    result = list(training = data_training, test = data_test)
     return(result)
-}
-
-print_atributes_parameters= function(prob, name = "") {
-    message(name)
-    message("min :", min(prob))
-    message("max :", max(prob))
-    message("mean :", mean(prob))
-    message("sd :", sd(prob))
 }
 
 get_cluster_parameters = function(data, model) {
     centers = model$centers
     distance = matrix()
     for (i in 1:nrow(model$centers))
-        distance[i] = mean(dist(data[model$cluster == i,]))
+        distance[i] = mean(dist(data[model$cluster == i, ]))
 
     df = list(centers = centers, distance = distance)
     return(df)
@@ -54,52 +49,49 @@ get_cluster_parameters = function(data, model) {
 
 record_in_known_clusters = function(element, params) {
     for (i in 1:nrow(params$centers)) {
-        distance = dist(rbind(element, params$centers[i,]))
+        distance = dist(rbind(element, params$centers[i, ]))
         if (distance <= params$distance[i])
             return(TRUE)
     }
     return(FALSE)
 }
 
-message("START")
+calculate_success = function(data_test, clusters_param) {
+    correct_decition = list(true_anomaly = 0, true_normal = 0)
 
-data = read_csv_file(creadit_card_10k)
-print(head(data, 3))
-message("Rate of frauds: ", sum(data$Class) / nrow(data))
+    for (i in 1:nrow(data_test$anoamaly))
+        correct_decition$true_anomaly = correct_decition$true_anomaly + (record_in_known_clusters(data_test$anoamaly[i,], clusters_param) == FALSE)
+    correct_decition$true_anomaly = correct_decition$true_anomaly / nrow(data_test$anoamaly)
 
-# first column is ID, second column is transaction time, last column is class
-data_x = data[,3:(ncol(data) - 1)]
-data_y = data[,ncol(data)]
+    for (i in 1:nrow(data_test$normal))
+        correct_decition$true_normal = correct_decition$true_normal + (record_in_known_clusters(data_test$normal[i, ], clusters_param) == TRUE)
+    correct_decition$true_normal = correct_decition$true_normal / nrow(data_test$normal)
 
-print(head(data_x, 3))
-print(head(data_y, 3))
-
-data_safe = data_x[data_y == 0,]
-data_fraud = data_x[data_y == 1,]
-
-message("Number of frauds: ", nrow(data_fraud))
-message("Number of good transactions: ", nrow(data_safe))
-
-calculate_mean = wrap_function_over_columns(mean)
-calculate_sd = wrap_function_over_columns(sd)
-
-means = calculate_mean(data_safe)
-deviations = calculate_sd(data_safe)
-
-prob_safe = independent_probability(data_safe, means, deviations)
-print_atributes_parameters(prob_safe, "safe")
-prob_fraud = independent_probability(data_fraud, means, deviations)
-print_atributes_parameters(prob_fraud, "fraud")
-
-model = kmeans(data_safe, 10)
-
-clusters_param = get_cluster_parameters(data_safe, model)
-
-detected_anomaly = 1:nrow(data_fraud)
-for (i in 1:nrow(data_fraud)) {
-    detected_anomaly[i] = (record_in_known_clusters(data_fraud[i,], clusters_param) == FALSE)
+    return(correct_decition)
 }
 
-message("Detected anomaly ", sum(detected_anomaly), " out of ", nrow(data_fraud))
+message("START")
+
+source("src/config.R")
+
+data_raw = read_csv_file(creadit_card_10k)
+print(head(data_raw, 3))
+
+normal_anomaly = extract_noraml_anomaly(data_raw)
+
+message("Number of anomaliess: ", nrow(normal_anomaly$anomaly))
+message("Number of normal: ", nrow(normal_anomaly$normal))
+
+training_test = extract_training_test(normal_anomaly)
+data_training = training_test$training
+data_test = training_test$test
+
+model = kmeans(data_training$normal, 10)
+clusters_param = get_cluster_parameters(data_training$normal, model)
+
+correct_decition_rate = calculate_success(data_test, clusters_param)
+
+message("Success rate for anomaly: ", correct_decition_rate$true_anomaly)
+message("Success rate for normal: ", correct_decition_rate$true_normal)
 
 message("END")
